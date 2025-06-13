dbf46dd055741fbbcf62784809146056c104be96
JoeMazich <jmazich@luc.edu>
2025-06-13 16:36:32 -0500

    add lint, test pre-push

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': ['error', { singleQuote: true }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'no-restricted-imports': [
         'error',
