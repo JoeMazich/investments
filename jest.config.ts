fc1a4081a17ef6e1d7b1d2f392f6730554b1f8c0
JoeMazich <jmazich@luc.edu>
2025-06-17 16:16:05 -0500

    fix alises

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ export default {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@redux/(.*)$': '<rootDir>/src/redux/$1',
   },
   coverageThreshold: {
     global: {
